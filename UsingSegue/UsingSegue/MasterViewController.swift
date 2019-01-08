353defe14e41f271789bc674ff4b0b58906cbab9
admin <admin@MacBook-Pro.local>
2019-01-08 21:30:00 +0700

    Bat dau lam bai truyen du lei dung segue

---
+++ UsingSegue/UsingSegue/MasterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UsingSegue
+//
+//  Created by admin on 1/8/19.
+//  Copyright © 2019 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
